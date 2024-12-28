78fcf3c655446543cd5c8f5e291fec50b6e38c80
eli <ebezhiasvhili@gmail.com>
2024-12-28 15:47:58 +0400

    add minimized header sidebar to stay always visible

--- src/Components/Layout/Header/HeaderComponent.jsx
+++ src/Components/Layout/Header/HeaderComponent.jsx
@@ -11,7 +11,7 @@ import {
 import { CiLogin } from "react-icons/ci";
 
 function HeaderComponent() {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
 
   const toggle = () => {
     setOpen(!open);
@@ -19,105 +19,104 @@ function HeaderComponent() {
 
   return (
     <header>
-      <div className="text-center"></div>
       <div
-        className={`fixed top-0 left-0 z-40 w-64 h-screen p-4 overflow-y-auto bg-white transform ${
-          open ? "translate-x-0" : "-translate-x-full"
-        } transition-transform duration-300 ease-in-out`}
+        className={`fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto bg-white transition-all duration-300 ease-in-out ${
+          open ? "w-64" : "w-20"
+        }`}
       >
         <div className="py-4 overflow-y-auto">
-          <h1>Finance</h1>
-          <ul className="space-y-2 font-medium">
+          {/* Header Title */}
+          <h1 className="text-2xl font-bold">{open ? "Finance" : "F"}</h1>
+          <ul className="space-y-2 font-medium mt-6">
             <li>
               <a
                 href="/"
                 className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100"
-                onClick={() => setOpen(false)}
               >
-                <AiFillHome />
-                <span className="ms-3">Overview</span>
+                <AiFillHome className="text-lg" />
+                {open && <span className="ms-3">Overview</span>}
               </a>
             </li>
             <li>
               <a
                 href="/transactions"
                 className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100"
-                onClick={() => setOpen(false)}
               >
-                <TbArrowsDownUp />
-                <span className="ms-3 whitespace-nowrap">Transactions</span>
+                <TbArrowsDownUp className="text-lg" />
+                {open && (
+                  <span className="ms-3 whitespace-nowrap">Transactions</span>
+                )}
               </a>
             </li>
             <li>
               <a
                 href="/budget"
                 className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100"
-                onClick={() => setOpen(false)}
               >
-                <FcStatistics />
-                <span className=" ms-3 whitespace-nowrap">Budgets</span>
+                <FcStatistics className="text-lg" />
+                {open && (
+                  <span className="ms-3 whitespace-nowrap">Budgets</span>
+                )}
               </a>
             </li>
             <li>
               <a
                 href="/pots"
                 className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100"
-                onClick={() => setOpen(false)}
               >
-                <FaSackDollar />
-                <span className="ms-3 whitespace-nowrap">Pots</span>
+                <FaSackDollar className="text-lg" />
+                {open && <span className="ms-3 whitespace-nowrap">Pots</span>}
               </a>
             </li>
             <li>
               <a
                 href="/bills"
                 className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100"
-                onClick={() => setOpen(false)}
               >
-                <FaMoneyCheck />
-                <span className="ms-3 whitespace-nowrap">Recurring Bills</span>
+                <FaMoneyCheck className="text-lg" />
+                {open && (
+                  <span className="ms-3 whitespace-nowrap">
+                    Recurring Bills
+                  </span>
+                )}
               </a>
             </li>
           </ul>
-          <ul className="space-y-2 font-medium">
+          <ul className="space-y-2 font-medium mt-6">
             <li>
               <a
                 href="/LogIn"
                 className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100"
-                onClick={() => setOpen(false)}
               >
-                <CiLogin />
-                <span className="ms-3">LogIn</span>
+                <CiLogin className="text-lg" />
+                {open && <span className="ms-3">LogIn</span>}
               </a>
             </li>
             <li>
               <a
                 href="/SignUp"
                 className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100"
-                onClick={() => setOpen(false)}
               >
-                <CiLogin />
-                <span className="ms-3">SignUp</span>
+                <CiLogin className="text-lg" />
+                {open && <span className="ms-3">SignUp</span>}
               </a>
             </li>
           </ul>
         </div>
         <button
           className="flex items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute bottom-2.5 right-2.5"
-          onClick={() => setOpen(false)}
+          onClick={toggle}
         >
-          <TbArrowBigLeftLinesFilled />
-          <span>Minimize Menu</span>
+          {open ? (
+            <>
+              <TbArrowBigLeftLinesFilled />
+              <span className="ms-2">Minimize Menu</span>
+            </>
+          ) : (
+            <TbArrowBigRightLinesFilled />
+          )}
         </button>
       </div>
-
-      <button
-        className="flex items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute bottom-2.5 left-2.5"
-        onClick={toggle}
-      >
-        <TbArrowBigRightLinesFilled />
-        <span>Maximize Menu</span>
-      </button>
     </header>
   );
 }
