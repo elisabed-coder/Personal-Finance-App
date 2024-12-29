25ea59c66d169ab837f2911a487f3855407ffcfa
eli <ebezhiasvhili@gmail.com>
2024-12-29 13:31:31 +0400

    Add logout button in home

--- src/Components/Home/HomeComponent.jsx
+++ src/Components/Home/HomeComponent.jsx
@@ -2,11 +2,19 @@ import BudgetsComponent from "../Budgets/BudgetsComponent";
 import PotsComponent from "../Pots/PotsComponent";
 import TransactionsComponent from "../Transactions/TransactinosComponent";
 import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 function HomeComponent() {
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    localStorage.removeItem("authToken");
+    navigate("/");
+  };
+
   return (
     <div>
       <h1>home</h1>
+      <button onClick={handleLogOut}>Log out</button>
       <BudgetsComponent />
       <PotsComponent />
       <TransactionsComponent />
