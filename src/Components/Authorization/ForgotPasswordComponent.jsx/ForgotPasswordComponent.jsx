573d6578a751c2a1f1352ce028cfeecd491e7a5f
eli <ebezhiasvhili@gmail.com>
2025-01-02 01:36:48 +0400

    fix forgotpassword link

--- src/Components/Authorization/ForgotPasswordComponent.jsx/ForgotPasswordComponent.jsx
+++ src/Components/Authorization/ForgotPasswordComponent.jsx/ForgotPasswordComponent.jsx
@@ -2,29 +2,25 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Input, Button, Typography } from "@material-tailwind/react";
 import AuthCard from "../authCard";
+import { toast, ToastContainer } from "react-toastify";
 
 const ForgotPasswordComponent = () => {
   const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8000/api/forgotPassword",
+        "http://127.0.0.1:8000/api/forgotPassword/",
         { email }
       );
       if (response.data.success) {
-        setMessage(response.data.message);
-        setErrorMessage("");
+        toast.success(response.data.message);
       } else {
-        setErrorMessage(response.data.message);
-        setMessage("");
+        toast.error(response.data.message);
       }
     } catch (error) {
-      setErrorMessage("Something went wrong. Please try again later.");
-      setMessage("");
+      toast.error("Something went wrong. Please try again later.");
     }
   };
 
@@ -32,32 +28,28 @@ const ForgotPasswordComponent = () => {
     <AuthCard
       title="Forgot Password"
       subtitle="Enter your email to reset your password."
-      onSubmit={handleSubmit}
     >
-      <Typography variant="h6" color="blue-gray" className="-mb-3">
-        Your Email
-      </Typography>
-      <Input
-        size="lg"
-        type="email"
-        placeholder="Enter your email"
-        value={email}
-        id="email"
-        className="!border-t-blue-gray-200 focus:!border-t-gray-900"
-        labelProps={{
-          className: "before:content-none after:content-none",
-        }}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Button className="mt-6" fullWidth type="submit">
-        Submit
-      </Button>
-      {message && (
-        <p className="text-green-500 text-sm text-center mt-2">{message}</p>
-      )}
-      {errorMessage && (
-        <p className="text-red-500 text-sm text-center mt-2">{errorMessage}</p>
-      )}
+      <ToastContainer />
+      <div className="mb-1 flex flex-col gap-4 text-left">
+        <Typography variant="h6" color="blue-gray" className="-mb-3">
+          Your Email
+        </Typography>
+        <Input
+          size="lg"
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          id="email"
+          className="!border-t-blue-gray-200 focus:!border-t-gray-900"
+          labelProps={{
+            className: "before:content-none after:content-none",
+          }}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Button className="mt-6" fullWidth type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </div>
     </AuthCard>
   );
 };
