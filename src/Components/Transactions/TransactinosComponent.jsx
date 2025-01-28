03d9cf2de23a2def50525d4074f8e62f4ff2c388
Elisabed <134492535+elisabed-coder@users.noreply.github.com>
2025-01-28 21:05:11 +0400

    add pageheader in transaction component

--- src/Components/Transactions/TransactinosComponent.jsx
+++ src/Components/Transactions/TransactinosComponent.jsx
@@ -1,7 +1,14 @@
+import PageHeader from "../ReusableComponents/PageHeder";
+
 function TransactionsComponent() {
+  const handleOpen = () => {};
   return (
-    <div>
-      <h1>TransactionsComponent</h1>
+    <div className="flex flex-col ">
+      <PageHeader
+        headerText="Transactions"
+        buttonText="Add New Transaction"
+        buttonFunction={handleOpen}
+      />{" "}
     </div>
   );
 }
